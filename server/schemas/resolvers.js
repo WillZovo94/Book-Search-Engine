988fe3e41b576996a611adc852c9e3c4dcf3337d
William <bzfishmaster94@gmail.com>
2024-09-23 18:21:05 -0400

    Worked on the schema's folder, need to still do the resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+    
+}
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+const { qpl } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, 
+        password: String!): Auth
+
+        addUser(username: String!, 
+        email: String!, 
+        password: String!): Auth
+
+        saveBook(
+            bookId: String!,
+            authors: [String]!,
+            description: String!,
+            title: String!,
+            image: String,
+            link: String!
+        ): User
+
+        removeBook(BookId: String!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: String
+        user: User
+    }
+
+`;
+
+module.exports = typeDefs;
