fe3ec9cd0f65bfc3e83320b42ba7fb3a613b8649
William <bzfishmaster94@gmail.com>
2024-09-23 19:08:56 -0400

    Added front end files, deleted some files that were not needed, worked on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,5 +3,64 @@ const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
-    
-}
+    Query: {
+        // A resolver for the "me" query.
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findById(context.user._id).select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+    },
+    Mutation: {
+        // Resolver for login mutation
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        // Resolver for the addUser mutation
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        // Resolver for saveBook mutation
+        saveBook: async (parent, { bookId, authors, description, title, image, link }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    context.user._id,
+                    { $addToSet: { savedBooks: { bookId, authors, description, title, image, link } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        // Resolver for the removeBook mutation
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    context.user._id,
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+
+module.exports = resolvers;
